f9bffdd606ca6d722effe4f1a4c2153e1738b272
hoapooh <hoapooh2912@gmail.com>
2025-05-26 11:31:31 +0700

    Re-arrange and fix source code template

---
+++ src/features/admin/manage-something/ui/layouts/demo-layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DemoLayoutAdmin = () => {
+  return <div>DemoLayoutAdmin</div>;
+};
+
+export default DemoLayoutAdmin;
---
+++ src/features/client/home/ui/layouts/demo-layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DemoLayoutClient = () => {
+  return <div>DemoLayoutClient</div>;
+};
+
+export default DemoLayoutClient;
