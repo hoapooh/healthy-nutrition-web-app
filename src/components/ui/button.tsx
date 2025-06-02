b0a867417b400d2708bba7fae9fab072c702788c
hoapooh <hoapooh2912@gmail.com>
2025-06-02 17:43:13 +0700

    add small changes to manage unauthorized behavior

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/80",
         link: "text-primary underline-offset-4 hover:underline",
+        healthy: "bg-green-600 text-white shadow-xs hover:bg-green-600/80",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
